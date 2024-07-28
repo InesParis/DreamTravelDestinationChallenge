638880a47299ed3b8b211cc0c1960d39b8753d6d
InesParis <inesparisarranz@gmail.com>
2024-07-28 21:23:43 +0200

    first go
    starting to shape the css
    organising html
    js incorporating date and time

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("hello");
+function displayDate() {
+  let dateElement = document.querySelector("#date");
+  let date = moment().format("dddd Do, MMMM, YYYY");
+
+  dateElement.innerHTML = date;
+}
+function displayTime() {
+  let timeElement = document.querySelector("#time");
+  let time = moment().format("LTS");
+  timeElement.innerHTML = time;
+}
+setInterval(displayTime, 1000);
+displayDate();
